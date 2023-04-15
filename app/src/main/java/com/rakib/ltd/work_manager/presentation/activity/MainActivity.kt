fc70f882292afd5abe7cbb1f4afdf17a0eb381b3
Rakibcse99 <52864364+rakibcse99@users.noreply.github.com>
2023-04-15 16:45:02 +0600

    Initial commit

---
+++ app/src/main/java/com/rakib/ltd/work_manager/presentation/activity/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.rakib.ltd.work_manager.presentation.activity
+
+import android.os.Bundle
+import android.util.Log
+import androidx.work.*
+import com.rakib.ltd.work_manager.databinding.ActivityMainBinding
+import com.rakib.ltd.work_manager.presentation.base.BaseActivity
+import com.rakib.ltd.work_manager.worker.Workers
+import java.util.concurrent.TimeUnit
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun setBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun onViewReady(savedInstanceState: Bundle?) {
+        initializeWorkManager()
+    }
+
+    private fun initializeWorkManager() {
+        oneTimeWork()
+        //periodicWork()
+    }
+
+    private fun periodicWork() {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(false)
+            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+            .setRequiresCharging(false)
+            .setRequiresBatteryNotLow(true)
+            .build()
+        //minimum interval is 15-min, just wait 15 min
+        val myRequest = PeriodicWorkRequest.Builder(
+            Workers::class.java,
+            15,
+            TimeUnit.MINUTES
+        ).setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "work_id",
+                ExistingPeriodicWorkPolicy.KEEP,
+                myRequest
+            )
+    }
+
+    private fun oneTimeWork() {
+        val workManager = WorkManager.getInstance(this)
+        val myWorker = OneTimeWorkRequestBuilder<Workers>().build()
+        workManager.enqueueUniqueWork("worker1", ExistingWorkPolicy.REPLACE, myWorker)
+
+        workManager.getWorkInfoByIdLiveData(myWorker.id).observe(this) {
+            binding.tv.text = it.runAttemptCount.toString()
+            Log.d("WORKERS", "id = ${it.id}")
+            Log.d("WORKERS", "state.name = ${it.state.name}")
+            Log.d("WORKERS", "state.isFinished = ${it.state.isFinished}")
+            Log.d("WORKERS", "progress = ${it.progress}")
+            Log.d("WORKERS", "tags = ${it.tags}")
+            Log.d("WORKERS", "runAttemptCount = ${it.runAttemptCount}")
+        }
+
+        //cancel worker by UUId
+        /*
+        val workManager = WorkManager.getInstance(context)
+        val work = workManager.getWorkInfosByTag(MyWorker.TAG).get()
+        val uuid = work[0].id
+        workManager.cancelWorkById(uuid)
+        */
+    }
+}
---
+++ app/src/main/java/com/rakib/ltd/work_manager/worker/Workers.kt
@@ -0,0 +1,94 @@
+package com.rakib.ltd.work_manager.worker
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_MUTABLE
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Looper
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.CoroutineWorker
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.rakib.ltd.work_manager.R
+import com.rakib.ltd.work_manager.presentation.activity.MainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.Timer
+
+class Workers(context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+
+    companion object {
+        const val CHANNEL_ID = "channel_id"
+        const val NOTIFICATION_ID = 1
+    }
+
+    override fun doWork(): Result {
+        Log.d("success", "doWork: Success function called")
+        showNotification()
+
+        return try {
+            Result.success()
+        } catch (error: Exception) {
+//            if (runAttemptCount < 10) Result.retry() else Result.failure()
+            Result.failure()
+        }
+    }
+
+    private fun showNotification() {
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, FLAG_MUTABLE)
+
+        val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("New Task")
+            .setContentText("Subscribe on the channel")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Channel Name"
+            val channelDescription = "Channel Description"
+            val channelImportance = NotificationManager.IMPORTANCE_HIGH
+
+            val channel = NotificationChannel(CHANNEL_ID, channelName, channelImportance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        with(NotificationManagerCompat.from(applicationContext)) {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notify(NOTIFICATION_ID, notification.build())
+        }
+    }
+}
